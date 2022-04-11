ce1ed5f2c9751a399b42c5a109edbccd9483685e
violetbeach <ti6419@gmail.com>
2022-04-11 23:15:34 +0900

    Initial commit

---
+++ src/test/java/violetbeach/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,38 @@
+package violetbeach.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import violetbeach.querydsl.entity.Hello;
+import violetbeach.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+	@PersistenceContext
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+}
