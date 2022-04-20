ba9de17db88794e487cd719c5b472e21416c50b7
violetbeach <ti6419@gmail.com>
2022-04-20 22:22:56 +0900

    feat: Add entities

---
+++ src/main/java/violetbeach/querydsl/entity/Team.java
@@ -0,0 +1,29 @@
+package violetbeach.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name", "age"})
+public class Team {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    @OneToMany
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+}
