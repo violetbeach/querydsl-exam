c5b0aa305c05057fcb05c0c94616f1f343377e2c
Jerry(이재헌) <ljh@gabia.com>
2022-04-22 14:48:40 +0900

    test: Add entity test

---
+++ src/test/java/violetbeach/querydsl/entity/MemberTest.java
@@ -0,0 +1,45 @@
+package violetbeach.querydsl.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void testEntity() {
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member1", 20, teamA);
+
+        Member member3 = new Member("member1", 30, teamA);
+        Member member4 = new Member("member1", 40, teamA);
+
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.clear();
+
+    }
+
+}
