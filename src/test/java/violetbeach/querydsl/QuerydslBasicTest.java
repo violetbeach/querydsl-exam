478cabb051fc906002fe6fea1259e3879e018bab
Jerry(이재헌) <ljh@gabia.com>
2022-05-05 19:58:07 +0900

    test: 엔터티 관계 없이 조인 테스트 추가

--- src/test/java/violetbeach/querydsl/QuerydslBasicTest.java
+++ src/test/java/violetbeach/querydsl/QuerydslBasicTest.java
@@ -197,4 +197,18 @@ public class QuerydslBasicTest {
                 .containsExactly("teamA", "teamB");
     }
 
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .where(member.username.eq(team.name))
+                .fetch();
+        
+    }
+
 }
