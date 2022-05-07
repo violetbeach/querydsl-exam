b027b0b331035f8f601d9768f64509909caf0f6f
Jerry(이재헌) <ljh@gabia.com>
2022-05-07 19:39:14 +0900

    test: Dynamic Query (use BooleanBuilder) 테스트 추가

--- src/test/java/violetbeach/querydsl/QuerydslBasicTest.java
+++ src/test/java/violetbeach/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package violetbeach.querydsl;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.Projections;
@@ -25,6 +26,7 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
 import javax.transaction.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.querydsl.jpa.JPAExpressions.*;
@@ -416,6 +418,32 @@ public class QuerydslBasicTest {
         for(MemberDto memberDto : result) {
             System.out.println(memberDto);
         }
+    }
+
+    @Test
+    public void dynamicQuery_BooleanBuilder() {
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+        if(usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+
+        if(ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return queryFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
     }
 
 }
