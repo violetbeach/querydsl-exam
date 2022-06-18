32d22f27292fd8eca524a759f13490f3d8127c8b
Jerry(이재헌) <ti6419@gmail.com>
2022-06-18 21:14:01 +0900

    Member 조회 API 추가

---
+++ src/main/java/violetbeach/querydsl/controller/MemberController.java
@@ -0,0 +1,22 @@
+package violetbeach.querydsl.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import violetbeach.querydsl.dto.MemberSearchCondition;
+import violetbeach.querydsl.dto.MemberTeamDto;
+import violetbeach.querydsl.repository.MemberJpaRepository;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.searchByBuilder(condition);
+    }
+}
