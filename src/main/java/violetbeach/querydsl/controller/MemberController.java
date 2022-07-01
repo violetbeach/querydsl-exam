32107432fe998f9e846659424fe5ff9651099a0a
Jerry(이재헌) <ti6419@gmail.com>
2022-07-01 23:10:25 +0900

    add Controller (use Paging)

--- src/main/java/violetbeach/querydsl/controller/MemberController.java
+++ src/main/java/violetbeach/querydsl/controller/MemberController.java
@@ -1,6 +1,8 @@
 package violetbeach.querydsl.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import violetbeach.querydsl.dto.MemberSearchCondition;
@@ -15,8 +17,15 @@ public class MemberController {
 
     private final MemberRepository memberJpaRepository;
 
+
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.search(condition);
     }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberJpaRepository.searchPageSimple(condition, pageable);
+    }
+}
 }
